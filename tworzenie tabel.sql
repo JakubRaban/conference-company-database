d9312ffaedf2eef0053f9999102259636a47bb89
Jakub Raban <jakub.raban@gmail.com>
2019-01-05 20:03:44 +0100

    Nowy schemat

--- tworzenie tabel.sql
+++ tworzenie tabel.sql
@@ -1,5 +1,6 @@
 use dlugosz_a
 
+--utw
 create table Customers (
 	CustomerID int not null primary key identity(0,1),
 	Street varchar(74),
@@ -7,32 +8,77 @@ create table Customers (
 	AppartmentNumber int,
 	City varchar(34),
 	PostalCode char(6) check(PostalCode like '[0-9][0-9]-[0-9][0-9][0-9]'),
+)
+
+--utw
+create table Companies (
+	CompanyID int not null primary key foreign key references Customers(CustomerID),
+	CompanyName varchar(150) not null,
+	NIP char(10) unique not null,
 	Phone varchar(12) unique not null check(Phone like '+%'),
+	Email varchar(100)
+)
+
+--utw
+create table Participants (
+	ParticipantID int not null primary key identity(0,1),
+	FirstName varchar(30) not null,
+	LastName varchar(50) not null,
+	Phone varchar(15) unique check(Phone like '+%'),
 	Email varchar(50)
 )
 
-create table CompanyCustomers (
-	CustomerID int not null primary key foreign key references Customers(CustomerID),
-	CompanyName varchar(150) not null,
-	NIP char(10) unique not null
-)
-
+--utw
 create table PrivateCustomers (
-	CustomerID int not null primary key foreign key references Customers(CustomerID),
-	FirstName varchar(30) not null,
-	LastName varchar(50) not null,
-	PESEL char(11) unique not null
+	CustomerID int not null foreign key references Customers(CustomerID),
+	ParticipantID int not null foreign key references Participants(ParticipantID),
+	primary key clustered (CustomerID, ParticipantID)
 )
 
+--utw
+create table Student (
+	ParticipantID int not null primary key foreign key references Participants(ParticipantID),
+	StudentCardNumber varchar(10) not null
+)
+
+--utw
+create table EmployeesOfCompanies (
+	ParticipantID int not null foreign key references Participants(ParticipantID),
+	CompanyID int not null foreign key references Companies(CompanyID),
+	primary key clustered(ParticipantID, CompanyID)
+)
+
+--utw
+create table ConferenceOrders (
+	OrderID int not null primary key identity(0,1),
+	CustomerID int not null foreign key references Customers(CustomerID),
+	DateOrdered date not null,
+	DatePaid date
+)
+
+--utw
 create table Conferences (
 	ConferenceID int not null primary key identity(0,1),
-	CustomerID int not null foreign key references Customers(CustomerID),
+	OrderID int not null foreign key references ConferenceOrders(OrderID),
+	Name varchar(200) not null,
 	StartDate date not null,
 	EndDate date not null,
-	StudentDiscount real not null check(StudentDiscount between 0 and 1)
+	BasePrice money,
+	StudentDiscount real check(StudentDiscount between 0 and 1),
+	ParticipantsLimit int
 )
 
-create table Employees (
+--utw
+create table ConferencePricetables (
+	PriceID int not null primary key,
+	ConferenceID int not null foreign key references Conferences(ConferenceID),
+	PriceStartsOn date not null,
+	PriceEndsOn date not null,
+	DiscountRate real not null check(DiscountRate between 0 and 1),
+)
+
+--utw
+create table OurEmployees (
 	EmployeeID int not null primary key identity(0,1),
 	FirstName varchar(30) not null,
 	LastName varchar(50) not null,
@@ -43,90 +89,70 @@ create table Employees (
 	AppartmentNumber int,
 	City varchar(34),
 	PostalCode char(6) check(PostalCode like '[0-9][0-9]-[0-9][0-9][0-9]'),
-	Phone varchar(12) unique not null check(Phone like '+%')
+	Phone varchar(15) unique not null check(Phone like '+%')
 )
 
-create table ConferenceDay (
-	DayID int not null primary key identity(0,1),
+--utw
+create table ConferenceDays (
+	ConferenceDayID int not null primary key identity(0,1),
 	ConferenceID int not null foreign key references Conferences(ConferenceID),
-	Day date not null,
-	DayOrdinal smallint not null,
-	ParticipantsLimit int not null
+	Date date not null,
+	DayOrdinal smallint not null
 )
 
-create table Participants (
-	ParticipantID int not null primary key identity(0,1),
-	FirstName varchar(30) not null,
-	LastName varchar(50) not null,
-	Phone varchar(12) unique not null check(Phone like '+%'),
-	Email varchar(50)
+--utw
+create table ConferenceDayReservation (
+	DayReservationID int not null primary key identity(0,1),
+	ConferenceDayID int not null foreign key references ConferenceDays(ConferenceDayID),
+	ReservedSeats int not null
 )
 
+--utw
 create table ConferenceDayParticipants (
-	DayID int not null,
-	ParticipantID int not null,
-	primary key clustered (DayID, ParticipantID),
-	foreign key (DayID) references ConferenceDay(DayID),
-	foreign key (ParticipantID) references Participants(ParticipantID)
+	ConferenceDayParticipantID int not null primary key identity(0,1),
+	ConferenceDayID int not null foreign key references ConferenceDays(ConferenceDayID),
+	ParticipantID int not null foreign key references Participants(ParticipantID)
 )
 
+--utw
 create table ConferenceEmployees (
 	ConferenceID int not null,
 	EmployeeID int not null,
 	primary key clustered (ConferenceID, EmployeeID),
 	foreign key (ConferenceID) references Conferences(ConferenceID),
-	foreign key (EmployeeID)   references Employees(EmployeeID)
+	foreign key (EmployeeID)   references OurEmployees(EmployeeID)
 )
 
+--utw
 create table Workshops (
 	WorkshopID int not null primary key identity(0,1),
-	ConferenceDayID int not null foreign key references ConferenceDay(DayID),
 	Name varchar(100) not null,
+	Description varchar(1000)
+)
+
+--utw
+create table ConferenceDayWorkshops (
+	ConferenceDayWorkshopID int not null primary key identity(0,1),
+	ConferenceDayID int not null foreign key references ConferenceDays(ConferenceDayID),
+	WorkshopID int not null foreign key references Workshops(WorkshopID),
 	StartTime time not null,
 	EndTime time not null,
 	Price money not null,
-	ParticipantsLimit int not null
+	ParticipantsLimit int,
 )
 
-create table WorkshopParticipants (
-	ParticipantID int not null,
-	WorkshopID int not null,
-	primary key clustered (ParticipantID, WorkshopID),
-	foreign key (ParticipantID) references Participants(ParticipantID),
-	foreign key (WorkshopID)    references Workshops(WorkshopID)
-)
-
-create table ConferenceParticipants (
-	ConferenceID int not null,
-	ParticipantID int not null,
-	RegistrationDate date not null,
-	IsPaid bit not null,
-	primary key clustered (ConferenceID, ParticipantID),
-	foreign key (ConferenceID) references Conferences(ConferenceID),
-	foreign key (ParticipantID) references Participants(ParticipantID)
-)
-
-create table ConferencePricetable (
-	ConferenceDayID int not null primary key foreign key references ConferenceDay(DayID),
-	PriceValidFrom date not null,
-	PriceValidity int not null check(PriceValidity >= 0),  -- 0 czyli tylko dziœ, 1 czyli do jutra itd.
-	Price money not null
-)
-
-create table ConferenceDayReservation (
-	ConferenceDayID int not null,
-	CompanyCustomerID int not null,
-	NumberOfSeats int not null,
-	primary key clustered (ConferenceDayID, CompanyCustomerID),
-	foreign key (ConferenceDayID) references ConferenceDay,
-	foreign key (CompanyCustomerID) references CompanyCustomers(CustomerID)
-)
-
+--utw
 create table WorkshopReservation (
-	WorkshopID int not null,
-	CompanyCustomerID int not null,
-	NumberOfSeats int not null,
-	primary key clustered (WorkshopID, CompanyCustomerID),
-	foreign key (WorkshopID) references ConferenceDay,
-	foreign key (CompanyCustomerID) references CompanyCustomers(CustomerID)
+	WorkshopReservationID int not null primary key identity(0,1),
+	ConferenceDayWorkshopID int not null foreign key references ConferenceDayWorkshops(ConferenceDayWorkshopID),
+	ReservedSeats int not null
+)
+
+--utw
+create table WorkshopParticipants (
+	ConferenceDayParticipantID int not null, 
+	ConferenceDayWorkshopID int not null,
+	primary key clustered (ConferenceDayParticipantID, ConferenceDayWorkshopID),
+	foreign key (ConferenceDayParticipantID) references ConferenceDayParticipants(ConferenceDayParticipantID),
+	foreign key (ConferenceDayWorkshopID) references ConferenceDayWorkshops(ConferenceDayWorkshopID)
 )
