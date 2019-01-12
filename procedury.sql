a9b9332cd6776678690e25f2cb83aaf866ff851a
pawlooss1 <pawlooss13@gmail.com>
2019-01-12 20:13:44 +0100

    Nieparzyste zaczęte

--- procedury.sql
+++ procedury.sql
@@ -31,3 +31,44 @@ insert into Workshops (Name, Description) values (@Name, @Description)
 end
 go
 
+create procedure NewParticipant
+	@FirstName varchar(30),
+	@LastName varchar(50),
+	@Phone varchar(15),
+	@Email varchar(50)
+as
+begin
+insert into Participants (FirstName, LastName, Phone, Email) values (@FirstName, @LastName, @Phone, @Email)
+end
+go
+
+create procedure BoundParticipantWithCompany
+	@Phone varchar(15),
+	@NIP char(10)
+as
+begin
+insert into EmployeesOfCompanies (ParticipantID, CompanyID)
+values (
+	(select ParticipantID from Participants where Phone = @Phone),
+	(select CompanyID from Companies where NIP = @NIP)
+)
+end
+go
+
+create procedure NewConference
+	@Name varchar(200),
+	@StartDate date,
+	@EndDate date,
+	@BasePrice money,
+	@StudentDiscount real,
+	@ParticipantLimit int,
+	@Street varchar(74),
+	@HouseNumber varchar(5),
+	@AppartmentNumber int,
+	@City varchar(80),
+	@PostalCode char(6)
+as
+declare @CityID int;
+set @CityID = FindCity(CityName = @City);
+begin
+insert into 
