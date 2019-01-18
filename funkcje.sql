26ab5939af6fecad7dfba8bac81939954625502f
pawlooss1 <pawlooss13@gmail.com>
2019-01-18 01:15:56 +0100

    Poprawki funkcji

--- funkcje.sql
+++ funkcje.sql
@@ -30,33 +30,6 @@ begin
 end
 go
 
-create function FindParticipantByPhone (@Phone varchar(15))
-returns int
-as
-begin
-	return (select ParticipantID from Participants where Phone = @Phone)
-end
-go
-
-create function FindCustomerByPhone	(@Phone varchar(15))
-returns int
-as
-begin
-	declare @CompanyID int;
-	exec @CompanyID = FindCompanyByPhone @Phone;
-	if @CompanyID is null
-	begin
-		declare @ParticipantID int;
-		exec FindParticipantByPhone @Phone;
-		return (select CustomerID from PrivateCustomers where ParticipantID = @ParticipantID)
-	end
-	return @CompanyID
-end
-go
-
---create function CalculatePriceForReservation (
-
-
 CREATE FUNCTION ConferenceSize(@ConferenceDayID INT)
 RETURNS int
 BEGIN
